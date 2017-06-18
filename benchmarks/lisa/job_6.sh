0d905d0d288464ec8d572c9424c42ea3850dd14c
Jeroen van Hoof <jmapvhoof@gmail.com>
2017-06-18 22:42:02 +0200

    Created a job-subber.

--- benchmarks/lisa/job_6.sh
+++
@@ -1,6 +0,0 @@
-#PBS -lwalltime=2:00:00
-#PBS -lnodes=1
-#PBS -lnodes=1:cpu3
-
-# EI - Forest/extra
-~/python/python36/bin/python3 ~/Optimus/benchmark.py 145677 2 300 "forest" "extra forest" 5
--- benchmarks/lisa/job_subber.py
+++ benchmarks/lisa/job_subber.py
@@ -1,4 +1,46 @@
 import subprocess
+import os
 
-for i in range(0, 7):
-    subprocess.call(["qsub", "./job_{}.sh".format(i)])
+python_path = "/home/jhoof/python/python36/bin/python3"
+project_dir = "/home/jhoof/Optimus/"
+config = "#PBS -lwalltime=2:00:00\n#PBS -lnodes=1:cpu3\n"
+
+
+def create_all_jobs(task, seed):
+    # Randomized
+    create_job(task, 0, seed)
+
+    # Normal - GP
+    create_job(task, 1, seed, score_regressor="gp")
+
+    # Normal - Forest
+    create_job(task, 1, seed, score_regressor="forest")
+
+    # EI/s - GP/GP
+    create_job(task, 2, seed, score_regressor="gp", time_regressor="gp")
+
+    # EI/s - Forest / Forest
+    create_job(task, 2, seed, score_regressor="forest", time_regressor="forest")
+
+    # EI/s - Forest / Extra forest
+    create_job(task, 2, seed, score_regressor="forest", time_regressor="extra forest")
+
+    # EI/s - Forest - Linear
+    create_job(task, 2, seed, score_regressor="forest", time_regressor="linear")
+
+
+def get_number_of_jobs():
+    return len(os.listdir(project_dir + "benchmarks/lisa/jobs/"))
+
+
+def create_job(task, method, seed, score_regressor="gp", time_regressor="gp", starting_points=5):
+    n_jobs = get_number_of_jobs()
+    f = open('jobs/job_{}.sh'.format(n_jobs), "w+")
+    f.write(config)
+    benchmark_path = project_dir + "benchmark.py"
+    f.write("{0} {1} {2} {3} {4} \"{5}\" \"{6}\" {7}".format(python_path, benchmark_path, task, method, seed,
+                                                             score_regressor, time_regressor, starting_points))
+
+def sub_jobs():
+    for i in range(0, get_number_of_jobs()):
+        subprocess.call(["qsub", "./jobs/job_{}.sh".format(i)])
