c7762152389bd40207b2a6a843e2f7e9ef1906be
Jeroen van Hoof <jmapvhoof@gmail.com>
2017-07-03 22:01:43 +0200

    Added a robust setting converter, for if we gained some knowledge with another param distribution than that we want to search over now.

--- optimus_ml/optimizer/converter.py
+++ optimus_ml/optimizer/converter.py
@@ -4,16 +4,32 @@ import numpy as np
 
 class Converter:
     @staticmethod
-    def convert_settings(settings, param_distributions):
+    def convert_settings(settings, param_distributions, robust=False):
         """
-        Iteratively calls `convert_settings()` to convert all dictionaries to numerical lists of parameter values.
-        :param param_distributions: Dictionary of parameter distributions
-        :param settings: A list of dictionaries with parameter names as keys and parameter values as its values
-        :return: A list of numerical lists of parameter values
+        Iteratively calls `convert_settings()` to convert all settings to its numerical representations.
+
+        Parameters
+        ----------
+        settings: list
+            A list of dictionaries with parameter names as keys and parameter values as its values
+
+        param_distributions: dict
+            Dictionary of parameter distributions
+
+        robust: bool
+            Whether to use `convert_settings()` or `convert_settings_robust()`
+
+        Returns
+        -------
+        A list with a numerical representation for each setting
         """
+
         result = []
         for setting in settings:
-            result.append(Converter.convert_setting(setting, param_distributions))
+            if robust:
+                result.append(Converter.convert_setting_robust(setting, param_distributions))
+            else:
+                result.append(Converter.convert_setting(setting, param_distributions))
         return result
 
     @staticmethod
@@ -33,6 +49,73 @@ class Converter:
             settings_copy[key] = param_distributions[key].index(value)
 
         return list(settings_copy.values())
+
+    @staticmethod
+    def convert_setting_robust(setting, param_distributions):
+        """
+        Converts a setting to numbers, and automatically resolves issues where keys or values from the setting
+        do not agree with the param distribution.
+
+        For each parameter key in param_distributions, we check if the key appears in the setting, and if the
+        corresponding parameter value appears in the list of possible values from param_distributions. We resolve three
+        cases.
+
+        1) Key not in setting
+            We take the median index.
+
+        2) Key in setting, but value not in possible values
+            If the value is a number, we try to get the index of the closest number in the possible values.
+            If the value is not a number, we take the median index.
+
+        3) Key in setting and value in possible values
+            We take the index of the value.
+
+        Parameters
+        ----------
+        setting: dict
+            A dictionary with parameter names as keys and parameter values as its values
+
+        param_distributions: dict
+            Dictionary of parameter distributions (lists)
+
+        Returns
+        -------
+        Returns a numerical representation of the setting.
+        """
+        result = []
+
+        for key, possible_values in param_distributions.items():
+
+            # case 1: key not in setting
+            if key not in setting:
+                median_index = (len(possible_values) - 1) / 2
+                result.append(median_index)
+
+            # case 2: key in setting but value not in possible_values
+            elif setting[key] not in possible_values:
+
+                # If it is a number, try to find closest number
+                if isinstance(setting[key], (np.int_, np.float_, float, int)):
+                    closest = possible_values[0]
+                    shortest_distance = np.inf
+                    for i in possible_values:
+                        if isinstance(i, (np.int_, np.float_, float, int)):
+                            distance = np.abs(i - setting[key])
+                            if distance < shortest_distance:
+                                closest = i
+                                shortest_distance = distance
+                    index_closest = possible_values.index(closest)
+                    result.append(index_closest)
+                else:
+                    median_index = (len(possible_values) - 1) / 2
+                    result.append(median_index)
+
+            # case 3: key in setting and value in possible_values
+            else:
+                index = possible_values.index(setting[key])
+                result.append(index)
+
+        return result
 
     @staticmethod
     def readable_parameters(parameters):
