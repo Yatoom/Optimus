e31e702bf24947bafc28df50e372276becc0812e
Yatoom <jmapvhoof@gmail.com>
2018-11-06 06:12:01 +0100

    Improved grid search and learning curve for time/score estimation models.

--- experiments/grid_search.py
+++ experiments/grid_search.py
@@ -7,22 +7,27 @@ from sklearn.model_selection import GridSearchCV
 # Settings
 target = "score"
 file = "score-data.csv"
-out = "score_250_samples_erf2.csv"
-task = 16
-use_log = False
+out = "score_250_samples_erf3.csv"
+task = 12
+use_log = True
 scoring = "neg_mean_squared_error"
 n_samples = 250
 parameters = {
-    "max_leaf_nodes": [4, 16, 64, 128],
-    "max_depth": [3, 5, 10, None],
-    "max_features": ["sqrt", "log2", None],
-    # "boosting_type": ["gbdt", "dart"],
-    # "learning_rate": [0.1, 0.2, 0.3, 0.4, 0.5],
-    "n_estimators": [10, 25, 50, 100]
+    "num_leaves": [4, 8, 16],
+    "max_depth": [-1],
+    "boosting_type": ["gbdt", "dart"],
+    "learning_rate": [0.05, 0.1, 0.15],
+    "n_estimators": [10, 25, 50, 100],
+    "reg_alpha": [0, 0.10, 0.15, 0.20, 0.25]
 }
 
 # Load data
 frame = pd.read_csv(file, index_col=0)
+
+# Drop duplicates and impute max depth
+frame = frame.drop_duplicates()
+frame["max_depth_None"] = frame["max_depth"] == 'NoneType'
+frame["max_depth"] = frame["max_depth"].replace("NoneType", 20).astype(int)
 
 # Convert data
 frame = pd.get_dummies(frame)
@@ -43,7 +48,7 @@ Y = Y[indices]
 Y_LOG = Y_LOG[indices]
 
 # Run Grid Search
-estimator = ExtraTreesRegressor(criterion="mse")
+estimator = LGBMRegressor(verbose=-1, min_child_samples=1, objective="mse")
 clf = GridSearchCV(estimator=estimator, param_grid=parameters, cv=10, verbose=10, scoring=scoring)
 
 if use_log:
@@ -52,9 +57,9 @@ else:
     clf.fit(X, Y)
 
 # Gather results
-frame = pd.DataFrame(clf.cv_results_["params"])
-frame["fit_time"] = clf.cv_results_["mean_fit_time"]
-frame["target"] = clf.cv_results_["mean_test_score"]
-frame = frame.sort_values("target")
+results = pd.DataFrame(clf.cv_results_["params"])
+results["fit_time"] = clf.cv_results_["mean_fit_time"]
+results["target"] = clf.cv_results_["mean_test_score"]
+results = results.sort_values("target")
 
-frame.to_csv(out)
+results.to_csv(out)
--- experiments/learning_curve.py
+++ experiments/learning_curve.py
@@ -5,20 +5,53 @@ import matplotlib.pyplot as plt
 # Settings
 from lightgbm import LGBMRegressor
 from sklearn.model_selection import learning_curve
+from smac.epm.rf_with_instances import RandomForestWithInstances
 
-target = "time"
-file = "time-data.csv"
-task = 12
+target = "score"
+file = "score-data.csv"
+task = 41
 scoring = "neg_mean_squared_error"
+
+class Wrap(RandomForestWithInstances):
+    def __init__(self, types, bounds, *args, **kwargs):
+        super().__init__(types=types, bounds=bounds, *args, **kwargs)
+
+    def fit(self, X, y):
+        return self.train(X, y)
+
+    def predict(self, X: np.ndarray):
+        means, vars = super().predict(X)
+        return means
+
+    def get_params(self, deep=False):
+        return {
+            "types": self.types,
+            "bounds": self.bounds,
+        }
+
+    def set_params(self, deep=False, **kwargs):
+        pass
 
 # Full model
 # params = {'boosting_type': 'gbdt', 'learning_rate': 0.1, 'max_depth': -1, 'n_estimators': 100, 'num_leaves': 128}
 
 # Simple model
-params = {'boosting_type': 'gbdt', 'learning_rate': 0.2, 'max_depth': -1, 'n_estimators': 100, 'num_leaves': 4}
+# params = {'boosting_type': 'gbdt', 'learning_rate': 0.2, 'max_depth': -1, 'n_estimators': 100, 'num_leaves': 4}
+# params = {'boosting_type': 'gbdt', 'learning_rate': 0.15, 'max_depth': -1, 'n_estimators': 100, 'num_leaves': 4, "reg_alpha": 0.15}
+# params = {'boosting_type': 'gbdt', 'learning_rate': 0.15, 'max_depth': -1, 'n_estimators': 100, 'num_leaves': 4, "reg_alpha": 0.20}
+
+# Simple model for scoring
+params = {'boosting_type': 'gbdt', 'learning_rate': 0.15, 'max_depth': -1, 'n_estimators': 100, 'num_leaves': 4, "reg_alpha": 0}
 
 # Load data
 frame = pd.read_csv(file, index_col=0)
+
+# Drop duplicates, get task, and impute max depth
+frame = frame.drop_duplicates()
+frame = frame[frame["score"] != 0]
+frame = frame[frame["group"] == task]
+frame["max_depth_None"] = frame["max_depth"] == 'NoneType'
+frame["max_depth"] = frame["max_depth"].replace("NoneType", 20).astype(int)
 
 # Convert data
 frame = pd.get_dummies(frame)
@@ -29,11 +62,15 @@ Y_LOG = np.log(Y + 1)
 groups = np.array(frame["group"])
 unique_groups = np.unique(groups)
 
-est = LGBMRegressor(min_child_samples=1, **params)
+est = LGBMRegressor(min_child_samples=1, objective="mse", **params)
+# bounds_ = list(zip(X.min(axis=0), X.max(axis=0)))
+# types_ = [0 for _ in range(len(bounds_))]
+# est = Wrap(np.array(types_), np.array(bounds_))
 
+np.random.seed(42)
 train_sizes, train_scores, test_scores = learning_curve(est, X=X, y=Y_LOG, cv=10,
                                                         train_sizes=np.arange(50, 2000, 50),
-                                                        scoring="neg_mean_squared_log_error", shuffle=True)
+                                                        scoring="neg_mean_squared_error", shuffle=True)
 
 # Plot
 train_scores_mean = np.mean(train_scores, axis=1)
@@ -41,6 +78,7 @@ train_scores_std = np.std(train_scores, axis=1)
 test_scores_mean = np.mean(test_scores, axis=1)
 test_scores_std = np.std(test_scores, axis=1)
 plt.grid()
+# plt.ylim(-0.005, 0.001)
 
 plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
                  train_scores_mean + train_scores_std, alpha=0.1,
